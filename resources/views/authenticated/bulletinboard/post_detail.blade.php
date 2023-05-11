b85f63081b488ecc373c03a60d66261270e647f6
LAPTOP-IV8G8769\teru <te02.ro27@gmail.com>
2023-05-11 12:20:22 +0900

    投稿削除確認アラート

--- resources/views/authenticated/bulletinboard/post_detail.blade.php
+++ resources/views/authenticated/bulletinboard/post_detail.blade.php
@@ -9,7 +9,7 @@
           </div>
           <div>
             <span class="edit-modal-open" post_title="{{ $post->post_title }}" post_body="{{ $post->post }}" post_id="{{ $post->id }}">編集</span>
-            <a href="{{ route('post.delete', ['id' => $post->id]) }}">削除</a>
+            <a onclick='return confirm("本当に削除しますか？")' href="{{ route('post.delete', ['id' => $post->id]) }}">削除</a>
           </div>
         </div>
 
