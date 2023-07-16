93ac72681267490746562d7645474d9cde7a807c
LAPTOP-IV8G8769\teru <te02.ro27@gmail.com>
2023-07-16 17:54:31 +0900

    予約できるようになった

--- app/Calendars/General/CalendarView.php
+++ app/Calendars/General/CalendarView.php
@@ -67,6 +67,7 @@ class CalendarView{
         }else{
           if($startDay <= $day->everyDay() && $toDay > $day->everyDay()){
             $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px">受付終了</p>';
+            $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }else{
           $html[] = $day->selectPart($day->everyDay());
           }
