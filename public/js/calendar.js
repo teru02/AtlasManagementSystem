da2f90cead16401da42f9da9954067b81c2c9ac7
LAPTOP-IV8G8769\teru <te02.ro27@gmail.com>
2023-07-31 22:19:23 +0900

    jsファイル6.7行目valをtextに

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -3,8 +3,8 @@ $(function () {
     $('.js-modal').fadeIn();
     var reserve_day = $(this).attr('reserve_day');
     var reserve_part = $(this).attr('reserve_part');
-    $('.modal-inner-day').val(reserve_day);
-    $('.modal-inner-part').val(reserve_part);
+    $('.modal-inner-day').text(reserve_day);
+    $('.modal-inner-part').text(reserve_part);
     return false;
   });
   $('.js-modal-close').on('click', function () {
