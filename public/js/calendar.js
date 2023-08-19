4f542927df9266dc72147bcd13e02515ebbd84cc
LAPTOP-IV8G8769\teru <te02.ro27@gmail.com>
2023-08-19 16:55:41 +0900

    予約キャンセルモーダル

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -3,7 +3,7 @@ $(function () {
     $('.js-modal').fadeIn();
     var reserve_day = $(this).attr('reserve_day');
     var reserve_part = $(this).attr('reserve_part');
-    var post_id = $(this).attr('value');
+    var value = $(this).attr('value');
     $('.modal-inner-day').text(reserve_day);
     $('.modal-inner-part').text(reserve_part);
     $('.edit-modal-hidden').val(value);
