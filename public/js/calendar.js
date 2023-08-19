435b1a10a79b9cd99b8a448ca6e9b938858716e6
LAPTOP-IV8G8769\teru <te02.ro27@gmail.com>
2023-08-19 16:50:33 +0900

    予約キャンセル確認依頼

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -3,8 +3,10 @@ $(function () {
     $('.js-modal').fadeIn();
     var reserve_day = $(this).attr('reserve_day');
     var reserve_part = $(this).attr('reserve_part');
+    var post_id = $(this).attr('value');
     $('.modal-inner-day').text(reserve_day);
     $('.modal-inner-part').text(reserve_part);
+    $('.edit-modal-hidden').val(value);
     return false;
   });
   $('.js-modal-close').on('click', function () {
