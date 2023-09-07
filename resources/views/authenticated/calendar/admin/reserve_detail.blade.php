ede692a02b4096acfeb1558ff25eed482274f731
LAPTOP-IV8G8769\teru <te02.ro27@gmail.com>
2023-09-08 00:20:50 +0900

    予約詳細レイアウト　テーブル

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -4,15 +4,15 @@
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
     <p><span><?php echo date('Y年m月d日',strtotime($date)); ?></span><span class="ml-3">{{$part}}部</span></p>
-    <div class="h-75 border">
-      <table class="">
-        <tr class="text-center">
+    <div class=" reserve_detail_box">
+      <table class="reserve_detail_table">
+        <tr class="text-left">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
           <th class="w-25">場所</th>
         </tr>
           @foreach($reservePersons->users as $persons)
-          <tr class="text-center">
+          <tr class="text-left">
             <td class="w-25">{{$persons->id}}</td>
             <td class="w-25"><span>{{$persons->over_name}}</span><span>{{$persons->under_name}}</span></td>
             <td>リモート</td>
